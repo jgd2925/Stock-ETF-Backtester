c68eddb47a9a6025abdf5bdd6cdcace68439cad0
jgd2925 <jgd2925@gmail.com>
2026-04-27 20:53:17 +0900

    Update Home.tsx grid-cols value

--- artifacts/portfolio-backtester/src/pages/Home.tsx
+++ artifacts/portfolio-backtester/src/pages/Home.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { Grid } from '@mui/material';
 
-// Other imports and code
-
-function Home() {
+const Home = () => {
     return (
-        <div className="grid grid-cols-1 lg:grid-cols-[550px_1fr] gap-6">
-            {/* Other content */}
-        </div>
+        <Grid container className='py-4'>
+            <Grid item xs={12} className='mb-4'>
+                <h1>Welcome to the Portfolio Backtester</h1>
+            </Grid>
+            <Grid item className='grid grid-cols-1 lg:grid-cols-[550px_1fr]' xs={12} lg={12}>
+                {/* Other components */}
+            </Grid>
+        </Grid>
     );
-}
+};
 
 export default Home;
